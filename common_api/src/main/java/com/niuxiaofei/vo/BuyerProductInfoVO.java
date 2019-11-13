181e642e8da3ad420de00f6c5a4453f6797fc89c
niuxiaofei <1017346477@qq.com>
2019-11-13 20:33:27 +0800

    项目初始化

---
+++ common_api/src/main/java/com/niuxiaofei/vo/BuyerProductInfoVO.java
@@ -0,0 +1,14 @@
+package com.niuxiaofei.vo;
+
+import lombok.Data;
+
+import java.util.List;
+
+@Data
+public class BuyerProductInfoVO {
+
+    private String name;
+    private Integer type;
+    private List<FoodVO> foods;
+
+}
---
+++ eureka_server/src/main/java/com/niuxiaofei/RegistryServer.java
@@ -0,0 +1,14 @@
+package com.niuxiaofei;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+
+@EnableEurekaServer
+@SpringBootApplication
+public class RegistryServer {
+    public static void main(String[] args) {
+        SpringApplication.run(RegistryServer.class,args);
+    }
+}
